5fa5f8411fe52d6d461fc1f9c4aff39097551ab5
Sobrinth <hey@sobrinth.net>
2022-12-02 11:50:00 +0100

    Day02: Quick and dirty

---
+++ AdventOfCode2022.Test/Day2Test.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode2022.Test;
+
+using FluentAssertions;
+using Xunit.Abstractions;
+
+public class Day2Test
+{
+
+    private const string Teststring = """
+                        A Y
+                        B X
+                        C Z
+                        """;
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day2Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForTeststringOnePartOne()
+    {
+        var score = Day2.CalculateScorePartOne(Teststring);
+        score.Should().Be(15);
+    }  
+    
+    [Fact]
+    private void CalculatesSolutionForTeststringOnePartTwo()
+    {
+        var score = Day2.CalculateScorePartTwo(Teststring);
+        score.Should().Be(12);
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForDayOnePartOne()
+    {
+        var rounds = File.ReadAllText("./puzzledata/day2.txt");
+        var score = Day2.CalculateScorePartOne(rounds);
+        _testOutputHelper.WriteLine(score.ToString());
+    }
+
+    [Fact]
+    private void CalculatesSolutionForDayOnePartTwo()
+    {
+        var rounds = File.ReadAllText("./puzzledata/day2.txt");
+        var score = Day2.CalculateScorePartTwo(rounds);
+        _testOutputHelper.WriteLine(score.ToString());
+    }
+}
---
+++ AdventOfCode2022/day2.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2022;
+
+public class Day2
+{
+    public static int CalculateScorePartOne(string rounds)
+    {
+        var games = rounds.Split(Environment.NewLine)
+            .Select(x => x.Split(' '))
+            .Select(y => (y[0], y[1]))
+            .ToList();
+
+        var score = 0;
+        foreach (var (opponent, player) in games)
+        {
+            score += (opponent, player) switch
+            {
+                ("A", "X") => 4,
+                ("A", "Y") => 8,
+                ("A", "Z") => 3,
+                ("B", "X") => 1,
+                ("B", "Y") => 5,
+                ("B", "Z") => 9,
+                ("C", "X") => 7,
+                ("C", "Y") => 2,
+                ("C", "Z") => 6,
+                _ => 0
+            };
+        }
+
+        return score;
+    }
+
+    public static object CalculateScorePartTwo(string rounds)
+    {
+        var games = rounds.Split(Environment.NewLine)
+            .Select(x => x.Split(' '))
+            .Select(y => (y[0], y[1]))
+            .ToList();
+
+        var score = 0;
+        foreach (var (opponent, player) in games)
+        {
+            score += (opponent, player) switch
+            {
+                // Now it is: X=lose, Y=draw, Z=win
+                ("A", "X") => 3,
+                ("A", "Y") => 4,
+                ("A", "Z") => 8,
+                ("B", "X") => 1,
+                ("B", "Y") => 5,
+                ("B", "Z") => 9,
+                ("C", "X") => 2,
+                ("C", "Y") => 6,
+                ("C", "Z") => 7,
+                _ => 0
+            };
+        }
+
+        return score;
+    }
+}
