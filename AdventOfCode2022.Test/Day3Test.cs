b29d052f989c86d365026ee86d1fdf1c65e32cab
Sobrinth <hey@sobrinth.net>
2022-12-03 17:48:44 +0100

    Day03: Quick and dirty

---
+++ AdventOfCode2022.Test/Day3Test.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode2022.Test;
+
+using FluentAssertions;
+using Xunit.Abstractions;
+
+public class Day3Test
+{
+    private static readonly string[] Teststring = """
+                        vJrwpWtwJgWrhcsFMMfFFhFp
+                        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                        PmmdzqPrVvPwwTWBwg
+                        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                        ttgJtRGJQctTZtZT
+                        CrZsJsPPZsGzwwsLwLmpwMDw
+                        """.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day3Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    private void CalculatesPrioritySumExampleListPartOne()
+    {
+        var sum = Day3.PartOne(Teststring);
+
+        sum.Should().Be(157);
+    }
+    
+    [Fact]
+    private void CalculatesPrioritySumExampleListPartTwo()
+    {
+        var sum = Day3.PartTwo(Teststring);
+
+        sum.Should().Be(70);
+    }
+
+    [Fact]
+    private void CalculatesSolutionForDayOnePartOne()
+    {
+        var rucksacks = File.ReadAllLines("./puzzledata/day3.txt");
+        var result = Day3.PartOne(rucksacks);
+        _testOutputHelper.WriteLine(result.ToString());
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForDayOnePartTwo()
+    {
+        var rucksacks = File.ReadAllLines("./puzzledata/day3.txt");
+        var result = Day3.PartTwo(rucksacks);
+        _testOutputHelper.WriteLine(result.ToString());
+    }
+}
---
+++ AdventOfCode2022/Day3.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2022;
+
+public class Day3
+{
+    public static int PartOne(IEnumerable<string> rucksacks) =>
+        rucksacks.Select(rucksack =>
+                (rucksack[..(rucksack.Length / 2)],
+                    rucksack[(rucksack.Length / 2)..]))
+            .Select((compartments) => compartments.Item1.Intersect(compartments.Item2))
+            .Aggregate(0, (sum, badge) => sum += GetPriority(badge.First()));
+
+    public static int PartTwo(IEnumerable<string> rucksacks) =>
+        rucksacks.Chunk(3)
+            .Select(groups => groups[0].Intersect(groups[1]).Intersect(groups[2]))
+            .Aggregate(0, (sum, badge) => sum += GetPriority(badge.First()));
+
+    private static int GetPriority(char item)
+    {
+        return item - 'a' >= 0 // Is the item a lowercase character?
+            ? item - 'a' + 1  // Priorities 1-26
+            : item - 'A' + 27; // Priorities 27-52 (basically just a flipped version of ASCII
+    }
+}
