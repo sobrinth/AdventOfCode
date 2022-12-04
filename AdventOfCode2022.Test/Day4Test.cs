b11d875c4100f130ee2898e726611dc05bed3032
Sobrinth <hey@sobrinth.net>
2022-12-04 13:42:42 +0100

    Day04: Quick and dirty

---
+++ AdventOfCode2022.Test/Day4Test.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode2022.Test;
+
+using FluentAssertions;
+using Xunit.Abstractions;
+
+public class Day4Test
+{
+    private static readonly string[] Teststring = """
+                        2-4,6-8
+                        2-3,4-5
+                        5-7,7-9
+                        2-8,3-7
+                        6-6,4-6
+                        2-6,4-8
+                        """.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day4Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    private void CalculatesExampleDataPartOne()
+    {
+        var sum = Day4.PartOne(Teststring);
+
+        sum.Should().Be(2);
+    }
+    
+    [Fact]
+    private void CalculatesExampleDataPartTwo()
+    {
+        var sum = Day4.PartTwo(Teststring);
+
+        sum.Should().Be(4);
+    }
+
+    [Fact]
+    private void CalculatesSolutionForDayOnePartOne()
+    {
+        var input = File.ReadAllLines("./puzzledata/day4.txt");
+        var result = Day4.PartOne(input);
+        _testOutputHelper.WriteLine(result.ToString());
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForDayOnePartTwo()
+    {
+        var input = File.ReadAllLines("./puzzledata/day4.txt");
+        var result = Day4.PartTwo(input);
+        _testOutputHelper.WriteLine(result.ToString());
+    }
+}
---
+++ AdventOfCode2022/Day4.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2022;
+
+public class Day4
+{
+    public static int PartOne(IEnumerable<string> pairs) =>
+        pairs.Select(pair =>
+                pair.Split(',', '-')
+                    .Select(int.Parse)
+                    .ToArray())
+            .Count(x =>
+                (x[0] >= x[2] && x[1] <= x[3])
+                || (x[2] >= x[0] && x[3] <= x[1]));
+
+    public static int PartTwo(IEnumerable<string> pairs) =>
+        pairs.Select(pair =>
+                pair.Split(',', '-')
+                    .Select(int.Parse)
+                    .ToArray())
+            .Count(x =>
+                (x[0] < x[2] && x[1] >= x[2])
+                || (x[0] >= x[2] && x[0] <= x[3]));
+}
