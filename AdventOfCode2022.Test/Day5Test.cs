f911745455d0dcdd066cf040075dde6a15c16970
Sobrinth <hey@sobrinth.net>
2022-12-05 10:06:36 +0100

    Day05: Quick and dirty

---
+++ AdventOfCode2022.Test/Day5Test.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode2022.Test;
+
+public class Day5Test
+{
+    private static readonly string Teststring = """
+                            [D]    
+                        [N] [C]    
+                        [Z] [M] [P]
+                         1   2   3 
+                        
+                        move 1 from 2 to 1
+                        move 3 from 1 to 3
+                        move 2 from 2 to 1
+                        move 1 from 1 to 2
+                        """;
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day5Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    private void CalculatesExampleDataPartOne()
+    {
+        var sum = Day5.PartOne(Teststring);
+
+        sum.Should().Be("CMZ");
+    }
+    
+    [Fact]
+    private void CalculatesExampleDataPartTwo()
+    {
+        var sum = Day5.PartTwo(Teststring);
+
+        sum.Should().Be("MCD");
+    }
+
+    [Fact]
+    private void CalculatesSolutionForPartOne()
+    {
+        var input = File.ReadAllText("./puzzledata/day5.txt");
+        var result = Day5.PartOne(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be("NTWZZWHFV");
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForPartTwo()
+    {
+        var input = File.ReadAllText("./puzzledata/day5.txt");
+        var result = Day5.PartTwo(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be("BRZGFVBTJ");
+    }
+}
---
+++ AdventOfCode2022/Day5.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode2022;
+
+using System.Text.RegularExpressions;
+
+public partial class Day5
+{
+    public static string PartOne(string teststring)
+    {
+        var strings = teststring.Split($"{Environment.NewLine}{Environment.NewLine}");
+        var crateStacks = CreateCrateStacks(strings[0]);
+
+        foreach (var command in strings[1].Split(Environment.NewLine))
+        {
+            var regexResult = MoveRegex().Match(command);
+            var moveCount = int.Parse(regexResult.Groups[1].Value);
+            var from = int.Parse(regexResult.Groups[2].Value) - 1;
+            var to = int.Parse(regexResult.Groups[3].Value) - 1;
+            
+            // Move crates
+            for (var i = 0; i < moveCount; i++)
+            {
+                crateStacks[to].Push(crateStacks[from].Pop());
+            }
+        }
+
+        return string.Join("", crateStacks.Select(stack => stack.Pop()));
+    }
+
+    public static object PartTwo(string teststring)
+    {
+        var strings = teststring.Split($"{Environment.NewLine}{Environment.NewLine}");
+        var crateStacks = CreateCrateStacks(strings[0]);
+
+        foreach (var command in strings[1].Split(Environment.NewLine))
+        {
+            var regexResult = MoveRegex().Match(command);
+            var moveCount = int.Parse(regexResult.Groups[1].Value);
+            var from = int.Parse(regexResult.Groups[2].Value) - 1;
+            var to = int.Parse(regexResult.Groups[3].Value) - 1;
+            
+            // Move crates
+            var carryStack = new Stack<char>(50);
+            for (var i = 0; i < moveCount; i++)
+            {
+                carryStack.Push(crateStacks[from].Pop());
+            }            
+            for (var i = 0; i < moveCount; i++)
+            {
+                crateStacks[to].Push(carryStack.Pop());
+            }
+        }
+
+        return string.Join("", crateStacks.Select(stack => stack.Pop()));
+    }
+
+    private static Stack<char>[] CreateCrateStacks(string problemHeader)
+    {
+        var stackLines = problemHeader.Split(Environment.NewLine).Reverse();
+        var stacks = stackLines.First().Chunk(4).Select(i => new Stack<char>(50)).ToArray();
+
+        foreach (var crateLine in stackLines.Skip(1))
+        {
+            foreach (var (stack, item) in stacks.Zip(crateLine.Chunk(4)))
+            {
+                if (!char.IsWhiteSpace(item[1]))
+                {
+                   stack.Push(item[1]); 
+                }
+            }
+        }
+
+        return stacks;
+    }
+
+    [GeneratedRegex("move (.*) from (.*) to (.)")]
+    private static partial Regex MoveRegex();
+}
