84098b2b31a87930e204c1c3a45981e74ed0667b
Sobrinth <hey@sobrinth.net>
2022-12-06 08:17:16 +0100

    Day06: Quick and dirty

---
+++ AdventOfCode2022.Test/Day6Test.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode2022.Test;
+
+public class Day6Test
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day6Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Theory]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    private void CalculatesExampleDataPartOne(string data, int startIndex)
+    {
+        var sum = Day6.PartOne(data);
+
+        sum.Should().Be(startIndex);
+    }
+    
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    private void CalculatesExampleDataPartTwo(string data, int startIndex)
+    {
+        var sum = Day6.PartTwo(data);
+
+        sum.Should().Be(startIndex);
+    }
+
+    [Fact]
+    private void CalculatesSolutionForPartOne()
+    {
+        var input = File.ReadAllText("./puzzledata/day6.txt");
+        var result = Day6.PartOne(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be(1920);
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForPartTwo()
+    {
+        var input = File.ReadAllText("./puzzledata/day6.txt");
+        var result = Day6.PartTwo(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be(2334);
+    }
+}
---
+++ AdventOfCode2022/Day6.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2022;
+
+public class Day6
+{
+    public static int PartOne(string teststring, int sequenceLength = 4)
+    {
+
+        var charWindow = new Queue<char>(sequenceLength + 1);
+        for (var i = 0; i < teststring.Length; i++)
+        {
+            charWindow.Enqueue(teststring[i]);
+            if (charWindow.Count > sequenceLength)
+            {
+                charWindow.Dequeue();
+            }
+
+            if (charWindow.Distinct().Count() == sequenceLength)
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int PartTwo(string teststring)
+    {
+        return PartOne(teststring, 14);
+    }
+}
