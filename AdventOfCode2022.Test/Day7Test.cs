34adf4d0bba36d1504d49ae820f097fa90d0cbdd
Sobrinth <hey@sobrinth.net>
2022-12-07 09:04:40 +0100

    Day07: Quick and dirty

---
+++ AdventOfCode2022.Test/Day7Test.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2022.Test;
+
+public class Day7Test
+{
+    private static readonly string Teststring = """
+                        $ cd /
+                        $ ls
+                        dir a
+                        14848514 b.txt
+                        8504156 c.dat
+                        dir d
+                        $ cd a
+                        $ ls
+                        dir e
+                        29116 f
+                        2557 g
+                        62596 h.lst
+                        $ cd e
+                        $ ls
+                        584 i
+                        $ cd ..
+                        $ cd ..
+                        $ cd d
+                        $ ls
+                        4060174 j
+                        8033020 d.log
+                        5626152 d.ext
+                        7214296 k
+                        """;
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day7Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    private void CalculatesExampleDataPartOne()
+    {
+        var sum = Day7.PartOne(Teststring);
+
+        sum.Should().Be(95437);
+    }
+    
+    [Fact]
+    private void CalculatesExampleDataPartTwo()
+    {
+        var sum = Day7.PartTwo(Teststring);
+
+        sum.Should().Be(24933642);
+    }
+
+    [Fact]
+    private void CalculatesSolutionForPartOne()
+    {
+        var input = File.ReadAllText("./puzzledata/day7.txt");
+        var result = Day7.PartOne(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be(1367870);
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForPartTwo()
+    {
+        var input = File.ReadAllText("./puzzledata/day7.txt");
+        var result = Day7.PartTwo(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be(549173);
+    }
+}
---
+++ AdventOfCode2022/Day7.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode2022;
+
+public class Day7
+{
+    public static int PartOne(string puzzleData)
+    {
+        return GetDirectories(puzzleData)
+            .Where(x => x.Size <= 100000)
+            .Sum(x => x.Size);
+    }
+
+    public static int PartTwo(string teststring)
+    {
+        const int total = 70000000;
+        const int neededUnused = 30000000;
+
+        var directories = GetDirectories(teststring);
+
+        var minSizeToDelete = neededUnused - (total - directories[0].Size); // Directories[0] is '/'
+
+        return directories
+            .Where(x => x.Size > minSizeToDelete)
+            .Min(x => x.Size);
+    }
+
+    private static List<Directory> GetDirectories(string teststring)
+    {
+        var lines = teststring.Split(Environment.NewLine);
+        var currentPath = new Stack<string>(10);
+        var directories = new List<Directory>(200);
+
+        foreach (var line in lines)
+        {
+            switch (line)
+            {
+                case "$ cd ..":
+                    currentPath.Pop();
+                    break;
+                case var _ when line.StartsWith("$ cd"):
+                    var targetDir = line.Split(' ').Last();
+                    currentPath.Push(targetDir);
+                    var dirName = string.Join('/', currentPath);
+                    if (directories.All(x => x.Path != dirName))
+                    {
+                        directories.Add(new Directory { Path = dirName });
+                    }
+                    break;
+                case var _ when !line.StartsWith("dir") && !line.StartsWith("$ "):
+                    directories.Where(x => string.Join('/', currentPath).Contains(x.Path)).ToList()
+                        .ForEach(x => x.Size += int.Parse(line.Split(' ')[0]));
+                    break;
+            }
+        }
+
+        return directories;
+    }
+
+    private class Directory
+    {
+        public required string Path { get; set; }
+        public int Size { get; set; }
+    }
+}
