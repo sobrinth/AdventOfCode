cd057e4383c47c4a50ee935a2cf6e8beb2fce0af
Sobrinth <hey@sobrinth.net>
2022-12-08 15:42:21 +0100

    Day08

---
+++ AdventOfCode2022.Test/Day8Test.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2022.Test;
+
+public class Day8Test
+{
+    private static readonly string Teststring = """
+                        30373
+                        25512
+                        65332
+                        33549
+                        35390
+                        """;
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day8Test(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    private void CalculatesExampleDataPartOne()
+    {
+        var sum = Day8.PartOne(Teststring);
+
+        sum.Should().Be(21);
+    }
+    
+    [Fact]
+    private void CalculatesExampleDataPartTwo()
+    {
+        var sum = Day8.PartTwo(Teststring);
+
+        sum.Should().Be(8);
+    }
+
+    [Fact]
+    private void CalculatesSolutionForPartOne()
+    {
+        var input = File.ReadAllText("./puzzledata/day8.txt");
+        var result = Day8.PartOne(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be(1807);
+    }
+    
+    [Fact]
+    private void CalculatesSolutionForPartTwo()
+    {
+        var input = File.ReadAllText("./puzzledata/day8.txt");
+        var result = Day8.PartTwo(input);
+        _testOutputHelper.WriteLine(result.ToString());
+        result.Should().Be(480000);
+    }
+}
---
+++ AdventOfCode2022/Day8.cs
@@ -0,0 +1,121 @@
+﻿namespace AdventOfCode2022;
+
+public class Day8
+{
+    private static List<int[]> ParseData(List<string> input)
+    {
+        List<int[]> data = new(50);
+        foreach (var s in input)
+        {
+            var arr = new int[s.Length];
+            for (var i = 0; i < s.Length; i++) arr[i] = s[i] - '0' + 1;
+            data.Add(arr);
+        }
+
+        return data;
+    }
+
+    public static int PartOne(string input)
+    {
+        var treeGrid = ParseData(input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList());
+        var dim = treeGrid.Count - 1;
+        var visible = treeGrid.Select(s => new int[treeGrid[0].Length]).ToList();
+        var length = treeGrid.Count;
+        var visibleTrees = 0;
+        for (var i = 0; i < length; i++)
+        {
+            var maxLeftRight = 0;
+            var maxRightLeft = 0;
+            var maxTopBot = 0;
+            var maxBotTop = 0;
+            for (var j = 0; j < length; j++)
+            {
+                if (treeGrid[i][j] > maxLeftRight)
+                {
+                    maxLeftRight = treeGrid[i][j];
+                    visible[i][j] = 1;
+                }
+
+                if (treeGrid[i][dim - j] > maxRightLeft)
+                {
+                    maxRightLeft = treeGrid[i][dim - j];
+                    visible[i][dim - j] = 1;
+                }
+
+                if (treeGrid[j][i] > maxTopBot)
+                {
+                    maxTopBot = treeGrid[j][i];
+                    visible[j][i] = 1;
+                }
+
+                if (treeGrid[dim - j][i] > maxBotTop)
+                {
+                    maxBotTop = treeGrid[dim - j][i];
+                    visible[dim - j][i] = 1;
+                }
+            }
+        }
+
+        for (var i = 0; i < length; i++)
+        {
+            for (var j = 0; j < length; j++)
+            {
+                visibleTrees += visible[i][j];
+
+            }
+        }
+        return visibleTrees;
+    }
+
+    public static int PartTwo(string input)
+    {
+        var treeGrid = ParseData(input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList());
+        var maxScenicScore = 0;
+        var len = treeGrid.Count;
+        for (var x = 0; x < len; x++)
+        {
+            for (var y = 0; y < len; y++)
+            {
+                var tmp = 0;
+                var scenicScore = 1;
+                for (var i = x - 1; i >= 0; i--)
+                {
+                    tmp++;
+                    if (treeGrid[i][y] >= treeGrid[x][y]) break;
+                }
+
+                scenicScore *= tmp;
+                tmp = 0;
+                for (var i = x + 1; i < len; i++)
+                {
+                    tmp++;
+                    if (treeGrid[i][y] >= treeGrid[x][y]) break;
+                }
+
+                scenicScore *= tmp;
+                tmp = 0;
+                for (var i = y - 1; i >= 0; i--)
+                {
+                    tmp++;
+                    if (treeGrid[x][i] >= treeGrid[x][y]) break;
+                }
+
+                scenicScore *= tmp;
+                tmp = 0;
+                for (var i = y + 1; i < len; i++)
+                {
+                    tmp++;
+                    if (treeGrid[x][i] >= treeGrid[x][y]) break;
+                }
+
+                scenicScore *= tmp;
+                if (scenicScore > maxScenicScore)
+                {
+                    maxScenicScore = scenicScore;
+                }
+            }
+        }
+
+        return maxScenicScore;
+    }
+}
